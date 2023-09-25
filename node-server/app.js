08123bc308ad3306bdec499335ea3712879a6f68
Shivam Kumar <132267516+shivam-kumar-07@users.noreply.github.com>
2023-09-26 00:34:23 +0530

    Update app.js

--- node-server/app.js
+++ node-server/app.js
@@ -17,6 +17,7 @@ const users = {};
 io.on('connection', (socket) => {
   socket.on('new-user-joined', (name) => {
     console.log(name);
+    console.log(socket.id);
     users[socket.id] = name;
     socket.broadcast.emit('user-joined', name);
   });
